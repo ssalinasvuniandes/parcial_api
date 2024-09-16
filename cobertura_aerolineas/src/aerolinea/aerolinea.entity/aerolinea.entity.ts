a54676b20d251f28feb907d908f1cf93705c6076
santiago alejandro salinas vargas <sanalejo1974@gmail.com>
2024-09-15 22:57:16 -0500

    se ajusto el contenido de las entity de aeropuerto y aerolinea

--- cobertura_aerolineas/src/aerolinea/aerolinea.entity/aerolinea.entity.ts
+++ cobertura_aerolineas/src/aerolinea/aerolinea.entity/aerolinea.entity.ts
@@ -1,1 +1,20 @@
-export class AerolineaEntity {}
+/* eslint-disable prettier/prettier */
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class AerolineaEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    nombre: string;
+
+    @Column()
+    descripcion: string;
+
+    @Column()
+    fecha_fundacion: string;
+
+    @Column()
+    pagina_web: string;
+}
--- cobertura_aerolineas/src/aeropuerto/aeropuerto.entity/aeropuerto.entity.ts
+++ cobertura_aerolineas/src/aeropuerto/aeropuerto.entity/aeropuerto.entity.ts
@@ -1,1 +1,20 @@
-export class AeropuertoEntity {}
+/* eslint-disable prettier/prettier */
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class AeropuertoEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    nombre: string;
+
+    @Column()
+    codigo: string;
+
+    @Column()
+    pais: string;
+
+    @Column()
+    ciudad: string;
+}
